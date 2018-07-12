ec1ecaf85d05087ef999d52799d6e96f20bc3fb4
Daniele Scasciafratte <mte90net@gmail.com>
2018-07-13 01:06:18 +0200

    ready for learning

--- 2 Test/remove_after_scrape.py
+++ 2 Test/remove_after_scrape.py
@@ -2,12 +2,8 @@
 import pandas as pd
 
 df=pd.read_csv('./analytics_content.csv', engine='python')
-print("CSV caricato")
-
-df = df.dropna()
-df = df.groupby('Pagina', as_index=False).aggregate({'Visualizzazioni di pagina': 'sum', 'Accessi': 'sum'}).reindex(columns=df.columns)
-df = df.drop(df.columns[0], axis=1)
 df = df[df['Pagina'] != '']
-
-df.to_csv('analytics_ready.csv')
+df.drop('Unnamed: 0.1', axis=1, inplace=True)
+df.dropna()
+df.to_csv('analytics_ready.csv', index=False)
 print("CSV pulito nuovamente")
