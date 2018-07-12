430b7caa484f85134ce2c7488b2a29b60dc59844
Daniele Scasciafratte <mte90net@gmail.com>
2018-07-12 19:22:29 +0200

    check with another column

--- analytics.py
+++ analytics.py
@@ -10,27 +10,19 @@ from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from yellowbrick.regressor import ResidualsPlot
-import numpy as np
 
-df=pd.read_csv('./analytics_parsed.csv', engine='python') 
-
-print("CSV caricato")
-
-rscaler = RobustScaler()
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(df['Pagina'])
-X = tokenizer.texts_to_matrix(df['Pagina'], mode='count')
-y = rscaler.fit_transform(df['Visualizzazioni di pagina'].values.reshape(-1, 1))
-print("Tonekizer caricato")
-
-# salvo gli errori
-model = LinearRegression()
-model.fit(X, y)
-dati = model.predict(X)
-df['Errors'] = dati
-df.drop(df.columns[[0]], axis=1, inplace=True)
-df.sort_values('Errors').to_csv('X.csv')
-print('Errori salvati')
+def save_column(df, column, file):
+    y = rscaler.fit_transform(df[column].values.reshape(-1, 1))
+    print("Tonekizer caricato")
+    
+    # salvo gli errori
+    model = LinearRegression()
+    model.fit(X, y)
+    dati = model.predict(X)
+    df['Errors'] = dati
+    df.sort_values('Errors').to_csv(file + '.csv')
+    print('Errori salvati')
+    return y
 
 def linearregression(X, y):
     model = LinearRegression()
@@ -56,7 +48,22 @@ def linearregression(X, y):
     print('Training score', training_score)
     print('Test score', test_score)
 
+
+df=pd.read_csv('./analytics_parsed.csv', engine='python') 
+df.drop(df.columns[[0]], axis=1, inplace=True)
+
+print("CSV caricato")
+
+rscaler = RobustScaler()
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(df['Pagina'])
+X = tokenizer.texts_to_matrix(df['Pagina'], mode='count')
+
+
 # dove si trova l'errore
+y = save_column(df, 'Visualizzazioni di pagina', 'X')
+save_column(df, 'Accessi', 'X_accessi')
+
 
 linearregression(X, y)
 linearregression(X, y)
