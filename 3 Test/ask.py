cf7e4cefbc7aeb8507ea20d74b56e3f4d2202eba
Daniele Scasciafratte <mte90net@gmail.com>
2018-07-13 12:25:18 +0200

    missing scaler

--- 3 Test/ask.py
+++ 3 Test/ask.py
@@ -2,6 +2,7 @@
 import os
 from keras.models import load_model
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import RobustScaler
 import numpy as np
 np.random.seed(1)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -9,6 +10,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def evaluate(X, name):
     model = load_model(name + '.h5')
+    rscaler = RobustScaler()
+    X = rscaler.fit_transform(X)
     predict = model.predict(X)
 
     score = r2_score(X, predict)
